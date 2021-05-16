764b42fbf7a4a2007d8e37711b500bd81872300c
IgorBanjac91 <igor.banjac91@gmail.com>
2021-05-16 09:58:39 +0200

    add dotenv-webpack plugin for hiding api keys

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   mode: 'development',
   entry: "./src/index.js",
+  plugins: [
+    new Dotenv()
+  ],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
