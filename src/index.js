69fdfd1eced25a2cf84c7bd5accc1a9c9c0f0dcc
IgorBanjac91 <igor.banjac91@gmail.com>
2021-05-17 12:42:30 +0200

    code function for fetching data

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import './reset.css';
 import './style.css';
+
+const img = document.querySelector("img");
+
+const apiKey = process.env.API_KEY;
+
+async function fetchWeatherDataFrom(location) {
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  const weatherInfo = data.weather[0];
+  const temperatures = data.main
+  const iconCode = weatherInfo.icon
+  console.log(temperatures.temp);
+  console.log(weatherInfo.main);
+
+  img.src = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+  img.alt = weatherInfo.description;
+}
+
+fetchWeatherDataFrom("trento");
