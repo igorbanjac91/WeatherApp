79116ca1f516789da86cc8322936c380f2ce0677
IgorBanjac91 <igor.banjac91@gmail.com>
2021-05-17 15:50:25 +0200

    refactor js code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
 import './reset.css';
 import './style.css';
 
-const img = document.querySelector("img");
-
 const apiKey = process.env.API_KEY;
 
-async function fetchWeatherDataFrom(location) {
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
+const img = document.querySelector(".weather-icon");
+const nameLocation = document.querySelector(".location");
+const temp = document.querySelector(".temp");
+const maxTemp = document.querySelector(".max-temp");
+const minTemp = document.querySelector(".min-temp");
+const input = document.querySelector("input");
+const searchBtn = document.querySelector("button");
 
+searchBtn.addEventListener("click", showWeather)
+
+async function fetchWeatherDatafor(location) {
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
   const response = await fetch(url);
-  const data = await response.json();
+  return await response.json();
+}
+
+function displayWeatherData(data) {
   const weatherInfo = data.weather[0];
   const temperatures = data.main
   const iconCode = weatherInfo.icon
-  console.log(temperatures.temp);
-  console.log(weatherInfo.main);
-
+  
+  nameLocation.textContent = data.name;
+  temp.textContent = `${Math.round(Number(temperatures.temp))}°`;
+  maxTemp.textContent = `${Math.round(Number(temperatures.temp_max))}°`;
+  minTemp.textContent = `${Math.round(Number(temperatures.temp_min))}°`;
   img.src = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
   img.alt = weatherInfo.description;
 }
 
-fetchWeatherDataFrom("trento");
+async function showWeather() {
+  let location = input.value || "Trento";
+  let data = await fetchWeatherDatafor(location)
+  displayWeatherData(data)
+}
+
+showWeather()
